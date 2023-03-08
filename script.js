5e2ad3921b5be3adef76fcbb30da31daa6d5a1b4
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-03-08 11:33:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const confirmation = confirm('Do you want to proceed?');
+if (confirmation) {
+  alert('You clicked OK. Proceeding...');
+} else {
+  alert('You clicked Cancel. Exiting...');
+}
